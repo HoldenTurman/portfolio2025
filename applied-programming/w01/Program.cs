43cbea24e9d289cf1981f93d0820814a0a8839e9
Holden Turman <holden.turman1@gmail.com>
2025-01-11 21:59:58 -0700

    Add files via upload

---
+++ applied-programming/w01/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System; 
+
+// namespace declaration 
+namespace HelloWorldApp { 
+    
+    // Class declaration 
+    class Geeks { 
+        
+        // Main Method 
+        static void Main(string[] args) { 
+            
+            // statement 
+            // printing Hello World! 
+            Console.WriteLine("Hello World!"); 
+        } 
+    } 
+}
